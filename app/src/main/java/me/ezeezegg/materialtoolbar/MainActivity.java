70a9b4b91ab2c7b3efb0f326f3f14912e6393405
Ezequiel Gonzalez Garcia <ezeezegg@gmail.com>
2015-03-18 12:58:53 -0600

    stable material toolbar without errors

--- app/src/main/java/me/ezeezegg/materialtoolbar/MainActivity.java
+++ app/src/main/java/me/ezeezegg/materialtoolbar/MainActivity.java
@@ -1,17 +1,23 @@
 package me.ezeezegg.materialtoolbar;
 
-import android.app.Activity;
+
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
+        setSupportActionBar(toolbar);                    // Setting toolbar as the ActionBar with setSupportActionBar() call
+
     }
 
 
